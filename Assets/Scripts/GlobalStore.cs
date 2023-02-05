ff24eed30b24ca8b807e5576f4d6502b469225d4
StaszeG <jacekgruchala1@gmail.com>
2023-02-05 12:45:49 +0100

    Adjust score

--- Assets/Scripts/GlobalStore.cs
+++ Assets/Scripts/GlobalStore.cs
@@ -8,6 +8,7 @@ public static class GlobalStore
 
     public static bool ShouldScrollScreen() => GameState == GameState.Running;
     public static int Score = 0;
+    public static int HighestScore = 0;
     public static Vector3 ObstacleVelocity { get { return new Vector3(-5 + -5 * Score / 100, 0, 0); } }
 }
 
